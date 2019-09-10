b742a35e8a3d6c38ce655c7669baca7bdbf4d0d4
yangzhukang <1072307269@qq.com>
2019-09-10 12:20:03 +0800

    机票详情页完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -5,19 +5,16 @@
             <!-- 顶部过滤列表 -->
             <div class="flights-content">
                 <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
+                <FlightsFilters :data="cacheFlightsData" @setDataList="setDataList"/>
                 
                 <!-- 航班头部布局 -->
                 <FlightsListHead/>
-                
                 
                 <!-- 航班信息 -->
                 <div>
                     <FlightsItem 
                     v-for="(item, index) in dataList"
-                    :key="index"
+                    :key="item.id"
                     :data="item"/>
 
                     <!-- 分页 -->
@@ -39,9 +36,7 @@
             </div>
 
             <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
+            <FlightsAside/>
         </el-row>
     </section>
 </template>
@@ -50,12 +45,26 @@
 
 import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
+import FlightsFilters from "@/components/air/flightsFilters.vue";
+import FlightsAside from "@/components/air/flightsAside.vue";
+
 
 export default {
     data(){
         return {
             // 机票列表返回的总数据，总数据包含4个属性，flights/info/options/tatol
-            flightsData: {},
+            flightsData: {
+                info: {},
+                options: {}
+            },
+
+            // 代表是大的数据，初始值和上面的flightsData是一样的，
+            // 这个变量一旦赋值之后不能再被修改
+            cacheFlightsData: {
+                info: {},
+                options: {}
+            },
+
             // 当前显示的列表数组
             dataList: [],
 
@@ -67,29 +76,64 @@ export default {
 
     components: {
         FlightsListHead,
-        FlightsItem
+        FlightsItem,
+        FlightsFilters,
+        FlightsAside
+    },
+
+    watch: {
+        // watch可以监听this下的所有属性
+        $route(){
+            // 请求航班列表数据
+            this.getData();
+        }
     },
 
     mounted(){
-
         // 请求航班列表数据
-        this.$axios({
-            url: "airs",
-            // 路由的url参数
-            params: this.$route.query
-        }).then(res => {
-            // 赋值给总数据
-            this.flightsData = res.data;
-
-            // 分页的总条数
-            this.total =  this.flightsData.flights.length;
-
-            // 第一页的值
-            this.dataList = this.flightsData.flights.slice(0, this.pageSize);
-        })
+       this.getData();
     },
 
     methods: {
+        getData(){
+             // 请求航班列表数据
+            this.$axios({
+                url: "airs",
+                // 路由的url参数
+                params: this.$route.query
+            }).then(res => {
+                // 赋值给总数据
+                this.flightsData = res.data;
+
+                // 赋值给缓存总数据
+                this.cacheFlightsData = {...res.data};
+
+                // 分页的总条数
+                this.total =  this.flightsData.flights.length;
+
+                // 第一页的值
+                this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+            })
+        },
+
+        // 该方法传递给子组件用于修改dataList
+        setDataList(arr){
+            // 修改总的航班列表
+            this.flightsData.flights = arr;
+
+            // 重新回到第一页
+            this.pageIndex = 1;
+
+            // 按照数学公式切换dataList的值
+            this.dataList = this.flightsData.flights.slice( 
+                (this.pageIndex - 1) * this.pageSize, 
+                this.pageIndex * this.pageSize 
+            );
+
+            // 修改总条数
+            this.total = arr.length;
+        },
+
         // 每页条数切换时候触发, val是条数
         handleSizeChange(val){
             this.pageSize = val;
